a6be310cd23eac8347be8cf467ef1406d8ac0537
Saptarshi Sarkar <67330269+saptarshisarkar20@users.noreply.github.com>
2026-01-17 23:53:05 +0530

    2195-TIME-NEEDED-TO-BUY-TICKETS Stats: Time: 0 ms (100%), Space: 42.9 MB (87.29%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -1,40 +1,13 @@
 class Solution {
-
-    public int timeRequiredToBuy(int[] tkt, int k) {
-        int ans = 0;
-        Queue<Pair<Integer, Boolean>> q = new ArrayDeque<>();
-        int temp_k = k;
-        for (int t : tkt) {
-            boolean flag = (temp_k == 0);
-            q.add(new Pair<>(t, flag));
-            temp_k--;
-        }
-
-        boolean done = false;
-        while (!q.isEmpty() && !done) {
-            int temp = q.peek().val;
-            boolean tg = q.peek().ind;
-            q.remove();
-
-            if (tg && temp == 1) return ++ans;
-
-            if (temp != 1) {
-                q.add(new Pair<>(temp - 1, tg));
+ public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0;
+        for(int i = 0; i<tickets.length; i++){
+            if(i<=k){
+                time+= Math.min(tickets[i], tickets[k]);
+            } else {
+                time += Math.min(tickets[i], tickets[k] - 1);
             }
-            ans++;
         }
-        return ans;
-
-
-    }
-
-    private static class Pair<V, K> {
-        public V val;
-        public K ind;
-
-        Pair(V v, K k) {
-            this.val = v;
-            this.ind = k;
-        }
+        return time;
     }
 }
